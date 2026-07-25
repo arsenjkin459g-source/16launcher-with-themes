01752e3935eec6bbe8ef0d9105e75f452b9c5e66
spacem0ney <arsenjkin459g@gmail.com>
2026-07-25 18:35:09 +0300

    Remove updater plugin to prevent crash

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -102,7 +102,6 @@ pub fn run() {
     let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .manage(pending_mrpack.clone())
